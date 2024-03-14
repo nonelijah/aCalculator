9d72e8ae43c606331dd202de0fe9efdbd78f1e28
nonelijah <elia341947@gmail.com>
2024-03-14 19:04:46 +0900

    add : js
    계산 기능을 아직 추가하지 않은 상황. 이후 추가 필요.

---
+++ js/calculator.js
@@ -0,0 +1,28 @@
+// 버튼
+const calculator = document.querySelector('.calculator');
+const screen = calculator.querySelector('.screen');
+const buttons = calculator.querySelector('.calculator-btns');
+const func = buttons.querySelectorAll('.func');
+const number = buttons.querySelectorAll('.number');
+const clearBtn = buttons.querySelector('#clear');
+const bracketBtn = buttons.querySelector('#bracket');
+const percentBtn = buttons.querySelector('#percent');
+const divideBtn = buttons.querySelector('#divide');
+const multiplyBtn = buttons.querySelector('#multiply');
+const subtractBtn = buttons.querySelector('#subtract');
+const addBtn = buttons.querySelector('#add');
+const changeBtn = buttons.querySelector('#change');
+const decimalPointBtn = buttons.querySelector('#decimal-point');
+const result = buttons.querySelector('#result');
+const num1 = buttons.querySelector('#number1');
+const num2 = buttons.querySelector('#number2');
+const num3 = buttons.querySelector('#number3');
+const num4 = buttons.querySelector('#number4');
+const num5 = buttons.querySelector('#number5');
+const num6 = buttons.querySelector('#number6');
+const num7 = buttons.querySelector('#number7');
+const num8 = buttons.querySelector('#number8');
+const num9 = buttons.querySelector('#number9');
+const num0 = buttons.querySelector('#number0');
+
+// 계산기
